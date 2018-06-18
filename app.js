e234fd00e0090265798c6c72ea8ca44f437117ef
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-18 18:53:38 +0530

    mongoDB connected sessions

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const app = require('express')();
+const session = require('express-session');
+const store = new MongoDBStore({
+    uri: 'mongodb://localhost:27017/grubx',
+    collection: 'sessions'
+});
+
+
 
 app.listen(3000, function () {
     console.log('Ecommerce sample listening on port 3000!');
