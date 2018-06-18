d401131eade18f6ed6c6fa0ac402135aa0bed4a5
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-18 23:37:30 +0530

    items route refactored to itemsAPI

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.get('/chillyPotato', (req, res)=> {
     res.send("product added");
     console.log(Cart);
 });
-app.get('/items', (req, res) => {
+app.get('/itemsAPI', (req, res) => {
     console.log("I am here");
     Product.find({}, function(err, items){
         if (err) console.log(err) ;
