0ad33bb2b894360dea7b11f7133de7c0c3014645
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-18 20:28:08 +0530

    session Cart

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const Security = require('./lib/security');
 //...
 
+let Cart;
 
 const store = new MongoDBStore({
     uri: 'mongodb://localhost:27017/grubx',
@@ -23,6 +24,7 @@ let checkCart = function (req,res,next){
     if (!req.session.cart) {
         req.session.cart = require('./lib/cart');
     }
+    Cart = req.session.cart;
     next();
 };
 app.use(checkCart);
